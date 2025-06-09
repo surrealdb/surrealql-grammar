eb15a07aaf2fcb3d3db5a5e0c3c1c738dee622c0
Julian Mills <julian.exodius@gmail.com>
2025-06-09 15:47:01 +0200

    initial LSP setup

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import type * as vscode from "vscode";
+import { workspace, window } from "vscode";
+import {
+	LanguageClient,
+	type LanguageClientOptions,
+	type ServerOptions,
+	TransportKind,
+} from "vscode-languageclient/node";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand(
-		"surrealql.helloWorld",
-		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage("Hello World from surrealql!");
+	const serverModule = require.resolve("@surrealdb/lsp/node");
+
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: { execArgv: ["--nolazy", "--inspect=6009"] },
 		},
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: "file", language: "surrealql" }],
+		synchronize: {
+			fileEvents: workspace.createFileSystemWatcher("**/*.{surql,surrealql}"),
+			configurationSection: "surrealql",
+		},
+	};
+
+	client = new LanguageClient(
+		"surrealql",
+		"SurrealQL Language Server",
+		serverOptions,
+		clientOptions,
 	);
 
-	context.subscriptions.push(disposable);
+	// Start the language client
+	client.start().catch((err) => {
+		window.showInformationMessage(
+			`SurrealQL Language Server failed to start: ${err.message}`,
+		);
+	});
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	return client?.stop();
+}
